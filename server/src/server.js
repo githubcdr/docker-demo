5f7543b3491e48900af3833cf7601229e32d105b
cDR <me@codar.nl>
2019-10-27 20:56:56 +0100

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,43 +1,22 @@
-let grpc = require("grpc");
-var protoLoader = require("@grpc/proto-loader");
-
-const server = new grpc.Server();
-const SERVER_ADDRESS = "0.0.0.0:5001";
-
-// Load protobuf
-let proto = grpc.loadPackageDefinition(
-  protoLoader.loadSync("protos/chat.proto", {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  })
-);
-
-let users = [];
-
-// Receive message from client joining
-function join(call, callback) {
-  users.push(call);
-  notifyChat({ user: "Server", text: "new user joined ..." });
+var PROTO_PATH = __dirname + '/proto/hello.proto';
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+function sayHello(call, callback) {
+  callback(null, {message: 'Hello ' + call.request.name});
 }
-
-// Receive message from client
-function send(call, callback) {
-  notifyChat(call.request);
+function main() {
+  var server = new grpc.Server();
+  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.start();
 }
-
-// Send message to all connected clients
-function notifyChat(message) {
-  users.forEach(user => {
-    user.write(message);
-  });
-}
-
-// Define server with the methods and start it
-server.addService(proto.example.Chat.service, { join: join, send: send });
-
-server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
-
-server.start();
+main();
