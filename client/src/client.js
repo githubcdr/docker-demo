7aa76818568c91197f24b35bb8ded2766c04d177
cDR <me@codar.nl>
2019-10-27 20:58:09 +0100

    Update client.js

--- client/src/client.js
+++ client/src/client.js
@@ -1,56 +1,28 @@
-let grpc = require("grpc");
-var protoLoader = require("@grpc/proto-loader");
-var readline = require("readline");
-
-//Read terminal Lines
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-//Load the protobuf
-var proto = grpc.loadPackageDefinition(
-  protoLoader.loadSync("protos/chat.proto", {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  })
-);
-
-const REMOTE_SERVER = "0.0.0.0:5001";
-
-let username;
-
-//Create gRPC client
-let client = new proto.example.Chat(
-  REMOTE_SERVER,
-  grpc.credentials.createInsecure()
-);
-
-//Start the stream between server and client
-function startChat() {
-  let channel = client.join({ user: username });
-
-  channel.on("data", onData);
-
-  rl.on("line", function(text) {
-    client.send({ user: username, text: text }, res => {});
+var PROTO_PATH = __dirname + '/proto/hello.proto';
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+const express = require('express')
+const app = express()
+const port = 3000
+function main() {
+  var client = new hello_proto.Greeter('grpc-server:50051', grpc.credentials.createInsecure());
+  var user;
+  app.get('/:id', function (req, res) {
+    user = req.params.id;
+    client.sayHello({name: user}, function(err, response) {
+      console.log('Greeting:', response.message);
+      res.send(response.message);
+    });
   });
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
-
-//When server send a message
-function onData(message) {
-  if (message.user == username) {
-    return;
-  }
-  console.log(`${message.user}: ${message.text}`);
-}
-
-//Ask user name then start the chat
-rl.question("What's ur name? ", answer => {
-  username = answer;
-
-  startChat();
-});
+main();
