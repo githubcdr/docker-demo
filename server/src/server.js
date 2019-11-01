d23404b5717c3d4b14bd7ea8db399f29ae97ad43
cDR <me@codar.nl>
2019-11-01 16:32:04 +0100

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -17,14 +17,8 @@ var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 function sayHello(call, callback) {
   callback(null, {message: 'Hello ' + call.request.name + ' from ' + process.env.HOSTNAME});
-
-  try {
-    await redis.set(call.request.name, 'Hello');
-  } catch (error) {
-    console.error(error);
-  }
+  redis.set(call.request.name, 'Hello');
   redis.disconnect();
-
   console.log('Greeting:', call.request.name);
 }
 
